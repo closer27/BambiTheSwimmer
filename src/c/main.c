05dc84291e23bc18325f28ed845eabcf078cce6c
Seungwon Kim <closer527@gmail.com>
2016-11-10 02:42:20 +0900

    add an animated image, change background color

--- src/c/main.c
+++ src/c/main.c
@@ -3,53 +3,13 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
-static void init() {
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
-
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
-
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
-}
-
-static void deinit() {
-  // Destroy Window
-  window_destroy(s_main_window);
-}
-
-static void main_window_load(Window *window) {
-  // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-}
-
-static void main_window_unload(Window *window) {
-  // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
-}
+static GBitmapSequence *s_sequence;
+static GBitmap *s_bitmap;
+static BitmapLayer *s_bitmap_layer;
 
 static void update_time() {
   // Get a tm structure
-  time_t temp = time(NULL);
+  time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
 
   // Write the current hours and minutes into a buffer
@@ -59,6 +19,112 @@ static void update_time() {
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void timer_handler(void *context) {
+  uint32_t next_delay;
+
+  // Advance to the next APNG frame, and get the delay for this frame
+  if(gbitmap_sequence_update_bitmap_next_frame(s_sequence, s_bitmap, &next_delay)) {
+    // Set the new frame into the BitmapLayer
+    bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+    layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
+
+    // Timer for that frame's delay
+    app_timer_register(next_delay, timer_handler, NULL);
+  } else {
+    gbitmap_sequence_restart(s_sequence);
+  }
+}
+
+static void load_sequence() {
+  if(s_sequence) {
+    gbitmap_sequence_destroy(s_sequence);
+    s_sequence = NULL;
+  }
+  if(s_bitmap) {
+    gbitmap_destroy(s_bitmap);
+    s_bitmap = NULL;
+  }
+
+  s_sequence = gbitmap_sequence_create_with_resource(RESOURCE_ID_ANIMATION);
+  s_bitmap = gbitmap_create_blank(gbitmap_sequence_get_bitmap_size(s_sequence), GBitmapFormat8Bit);
+
+  app_timer_register(1, timer_handler, NULL);
+}
+
+static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  window_set_background_color(window, (GColor8){ .argb = 0b11000001 });
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+      GRect(0, 106, bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  
+//   // Create sequence
+//   s_sequence = gbitmap_sequence_create_with_resource(RESOURCE_ID_BACKGROUND_IMAGE);
+
+//   // Create blank GBitmap using APNG frame size
+//   GSize frame_size = gbitmap_sequence_get_bitmap_size(s_sequence);
+//   s_bitmap = gbitmap_create_blank(frame_size, GBitmapFormat8Bit);
+  
+  // BitmapLayer - begin
+  s_bitmap_layer = bitmap_layer_create(GRect(42, 10, 98, 98));
+  bitmap_layer_set_background_color(s_bitmap_layer, (GColor8){ .argb = 0b11000001 });
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+  load_sequence();
+  // BitmapLayer - end
+  
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  
+  // Destroy Bitmaps
+  bitmap_layer_destroy(s_bitmap_layer);
+  gbitmap_sequence_destroy(s_sequence);
+  gbitmap_destroy(s_bitmap);
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
 }
 
 int main(void) {
